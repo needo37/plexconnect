7c656bc5874648413079aeaa892db35e57cc524b
needo <malcolm@joker.lan>
2014-06-24 14:53:08 -0500

    Remove continue

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Generate SSL certificates if they don't exist
 if [ -f /opt/plexconnect/assets/certificates/trailers.pem ] ; then
-  continue
+  echo "SSL certs exist"
 else
   openssl req -new -nodes -newkey rsa:2048 -out /opt/plexconnect/assets/certificates/trailers.pem -keyout /opt/plexconnect/assets/certificates/trailers.key -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
   openssl x509 -in /opt/plexconnect/assets/certificates/trailers.pem -outform der -out /opt/plexconnect/assets/certificates/trailers.cer && cat /opt/plexconnect/assets/certificates/trailers.key >> /opt/plexconnect/assets/certificates/trailers.pem
